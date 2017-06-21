bf3b1cd8a530b18481d0edc39a92a0dd90f61579
Bogdan Bucur <bogdan.bucur@udevoffice.ro>
2017-06-21 17:46:26 +0300

    It's done. (With a small issue on the last test, but whatever.)

--- src/AquaTest.py
+++ src/AquaTest.py
@@ -15,25 +15,30 @@ class AquaPOS(unittest.TestCase):
         desired_caps = {
             'platformName': 'Android',
             'platformVersion': '6.0',
-            'deviceName': 'Pixel C API 23',
-            'app': 'C:\Users\Bogdan Bucur\PycharmProjects\AquaPOSTest\src\AquaPOS.apk',
+            'deviceName': 'Samsung',
+            'app': 'C:\Users\BogdanBucur\PycharmProjects\AquaPOSTest\src\AquaPOS1.apk',
             'appPackage': 'com.udev.alidemirel.aquapos',
             'appActivity': 'activities.Register'
         }
 
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 
-    def test_1(self):
-        self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/codeText').click()
-        self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/codeText').send_keys('ebf58e09-84bb-37d3-b4d0-e7fc2ea270e1')
-        self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnRegister').click()
-
+    # # Register Device
+    # def test_1(self):
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/codeText').click()
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/codeText').send_keys("ebf58e09-84bb-37d3-b4d0-e7fc2ea270e1")
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnRegister').click()
+    # #
+    #
+    # # User Login
     # def test_2(self):
+    #     sleep(2)
     #     self.driver.find_element_by_name('User Carrefour 2').click()
     #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/userPin').click()
     #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/userPin').send_keys('0000')
     #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnLogin').click()
 
+    # Create Order with Cash Checkout
     # def test_3(self):
     #     sleep(2)
     #     self.driver.find_element_by_name('User Carrefour 2').click()
@@ -104,23 +109,114 @@ class AquaPOS(unittest.TestCase):
     #
     #     cart_list = self.driver.find_elements_by_xpath('//android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.LinearLayout[contains(@index, "1")]'
     #                                                    '/android.support.v7.widget.RecyclerView/android.widget.LinearLayout')
-    #     cart_size = self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/cartListSize').get_attribute('text')
+    #     cart_size = int(self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/cartListSize').get_attribute('text'))
     #
-    #     if len(cart_list) < int(cart_size):
-    #         for l in range(int(cart_size)):
+    #     # Increasing quantity while scrolling the list
+    #     if len(cart_list) >= cart_size:
+    #         for l in range(len(cart_list)):
     #             quantity = randint(1, 10)
     #             cart_product = self.driver.find_element_by_xpath('//android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.LinearLayout[contains(@index, "1")]'
     #                                                              '/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[contains(@index, "{0}")]'.format(str(l)))
     #             for m in range(quantity):
+    #
     #                 cart_product.find_element_by_id('com.udev.alidemirel.aquapos:id/addProduct').click()
     #
-    #             if l > (len(cart_list) - 1):
+    #     else:
+    #         o = 0
+    #         for n in range(cart_size):
+    #             quantity = randint(1, 10)
+    #             cart_product = self.driver.find_element_by_xpath('//android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.LinearLayout[contains(@index, "1")]'
+    #                                                              '/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[contains(@index, "{0}")]'.format(str(n - o)))
+    #             for m in range(quantity):
+    #                 cart_product.find_element_by_id('com.udev.alidemirel.aquapos:id/addProduct').click()
+    #
+    #             if n >= (len(cart_list) - 1):
     #                 self.driver.scroll(
     #                     self.driver.find_element_by_xpath('//android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.LinearLayout[contains(@index, "1")]'
     #                                                       '/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[contains(@index, "{0}")]'.format(len(cart_list) - 1)),
     #                     self.driver.find_element_by_xpath('//android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.LinearLayout[contains(@index, "1")]'
     #                                                       '/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[contains(@index, "{0}")]'.format(len(cart_list) - 2))
     #                 )
+    #                 o = o + 1
+    #
+    #     # Checkout
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnCheckout').click()
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnPaymentCash').click()
+    #     price = self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/viewTotalPrice').get_attribute('text')
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/enteredValue').click()
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/enteredValue').send_keys(price)
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnCompleteOrder').click()
+
+    # Create Advance Payment
+    # def test_4(self):
+    #     sleep(2
+    #           )
+    #     self.driver.find_element_by_name('User Carrefour 2').click()
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/userPin').click()
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/userPin').send_keys('0000')
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnLogin').click()
+    #
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnFilter').click()
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnAdvancePay').click()
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/enteredAdvance').click()
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/enteredAdvance').send_keys('100')
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/avansDesc').click()
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/avansDesc').send_keys('Advance Payment')
+    #     chance = randint(1, 2)
+    #     if chance == 1:
+    #         self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnCash').click()
+    #     else:
+    #         self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnCard').click()
+    #     self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnSubmit').click()
+
+    # Make a Refund on a Random Order
+    def test_5(self):
+        sleep(2)
+        self.driver.find_element_by_name('User Carrefour 2').click()
+        self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/userPin').click()
+        self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/userPin').send_keys('0000')
+        self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnLogin').click()
+        sleep(3)
+
+        self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnFilter').click()
+        self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnSales').click()
+        sleep(2)
+
+        orderIndex = randint(1, 5)
+        order = self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[contains(@index, "1")]'
+                                                  '/android.widget.LinearLayout[contains(@index, "0")]/android.support.v7.widget.RecyclerView/android.widget.LinearLayout'
+                                                  '[contains(@index, "{0}")]'.format(orderIndex))
+        order.click()
+
+        orderType = order.find_element_by_id('com.udev.alidemirel.aquapos:id/orderType').get_attribute('text')
+        if orderType == 'order':
+            self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnRefund').click()
+            sleep(1)
+            orderProductList = self.driver.find_elements_by_xpath('//android.widget.LinearLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout')
+            k = 0
+            for i in range(len(orderProductList)):
+                orderProduct = self.driver.find_element_by_xpath('//android.widget.LinearLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView'
+                                                                 '/android.widget.LinearLayout[contains(@index, "{0}")]'.format(i - k))
+
+                if int(orderProduct.find_element_by_id('com.udev.alidemirel.aquapos:id/productQuantity').get_attribute('text')) > 0:
+                    orderProductQuantity = int(orderProduct.find_element_by_id('com.udev.alidemirel.aquapos:id/productQuantity').get_attribute('text'))
+                    randomQuantity = randint(1, orderProductQuantity)
+                    self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/enterQuantity').click()
+                    self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/enterQuantity').send_keys(randomQuantity)
+
+                # if i == len(orderProductList) - 1:
+                #     self.driver.scroll(
+                #         self.driver.find_elements_by_xpath('//android.widget.LinearLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView'
+                #                                            '/android.widget.LinearLayout[contains(@index, "{0}")]'.format(len(orderProductList) - 2)),
+                #         self.driver.find_elements_by_xpath('//android.widget.LinearLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView'
+                #                                            '/android.widget.LinearLayout[contains(@index, "{0}")]'.format(len(orderProductList) - 3))
+                #     )
+                #     k = k + 1
+
+            sleep(2)
+            self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/textRefundDesc').click()
+            self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/textRefundDesc').send_keys('Pleb wants refund...')
+            self.driver.find_element_by_id('com.udev.alidemirel.aquapos:id/bttnRefund').click()
 
     def tearDown(self):
         self.driver.quit()
